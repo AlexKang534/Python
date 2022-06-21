ece89bf1f0d274f132395a961ece7678c61906c9
AlexKang <85082932+AlexKang534@users.noreply.github.com>
2022-06-20 20:23:41 -0700

    change file types to py

---
+++ madlib.py
@@ -0,0 +1,8 @@
+random = input("random: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+person = input("person: ") 
+
+madlib = f"Computer programming is so {random}! It gets increasingly obnoxious because I am trying to learn a new langauge \ when I think about it, I want to {verb1}. I should make sure that I {verb2} like you are {person}"
+
+print(madlib)
